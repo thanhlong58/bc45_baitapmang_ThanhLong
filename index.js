c10a20a849c633c2adda743e41a1128902e81c50
unknown <thannhlong5896@gmail.com>
2023-03-24 05:15:19 +0700

    'finish'

--- index.js
+++ index.js
@@ -63,40 +63,32 @@ document.getElementById('btnDemSoDuong').onclick = function () {
     var demSo = 0 ;
     viTriSoDuong = 0;
 
-
-    
     for (var index = 0 ; index <= arrNumber.length; index ++ ){
         if ( arrNumber[index] > 0 ) {
             viTriSoDuong = index;
             demSo ++
         }
-       
     }
-
     document.getElementById('ket-qua-2').innerHTML = demSo;
 }
 
 
 
 //tim so  nho nhat
-
 document.getElementById ('btnTimSoNhoNhat').onclick = function () {
     var min  =  arrNumber[0];
     for ( var index = 0 ; index <= arrNumber.length; index++) {
         if(arrNumber[index] < min) {
             min = arrNumber[index];
         }
-
     }
-
     document.getElementById('ket-qua-3').innerHTML  =  min;
 }
 
+ 
+
 
 //tim so duong nho nhat   
-
-
-
 document.getElementById('btnTimSoDuongNhoNhat').onclick = function () {
   
     
@@ -105,8 +97,7 @@ document.getElementById('btnTimSoDuongNhoNhat').onclick = function () {
     for (var index = 0; index < arrNumber.length; index++) {
         var min = arrSoDuong[0];
         if (arrNumber[index] > 0) {
-
-            arrSoDuong[index] = arrNumber[index];
+            arrSoDuong.push(arrNumber[index]);
         }
 
 
@@ -136,23 +127,11 @@ document.getElementById('btnTimSoChanCuoiCung').onclick = function () {
     for (var index = arrNumber.length -1 ; index >= 0 ; index--) {
 
         if ( arrNumber [index] % 2 === 0) {
-
-
           soChanCuoi = arrNumber[index];
-
           break;
-            
-            
-
         }
-
         // soChanCuoi = soChan;
-
-        
-
     }
-
-
     document.getElementById('ket-qua-5').innerHTML = soChanCuoi;
 
 
@@ -164,55 +143,30 @@ document.getElementById('btnDoiCho').onclick = function () {
 
     var viTriSo1 = +document.getElementById('vi-tri-1').value;
     var viTriSo2 = +document.getElementById('vi-tri-2').value;
-
     var giaTri1 = 0;
     var giaTri2 = 0;
-
-
     //output 
 
     //process
     for (var index = 0; index < arrNumber.length; index++) {
-
-
-
         if (viTriSo1 == index) {
-
             var giaTri3 = arrNumber[viTriSo1];
             giaTri1 = arrNumber[index];
-
             arrNumber[viTriSo1] = arrNumber[viTriSo2];
-
         }
-
         if (viTriSo2 == index) {
-
             arrNumber[viTriSo2] = giaTri3;
-
         }
-
-
-
-        document.getElementById('ket-qua-6').innerHTML = arrNumber;
-
     }
+    document.getElementById('ket-qua-6').innerHTML = arrNumber;
 }
 
 
 //sắp xếp mảng theo thứ tự tăng dần  //đang  cấn
 document.getElementById('btnSapXep').onclick = function () {
-   for (var i  = 0 ;i < arrNumber.length; i++) {
-     for (var j = 0; j -i - 1; j++) {
-        if (arrNumber[j] > arrNumber[j + 1]) {
-            var temp  = arrNumber[j];
-            arrNumber[j]  = arrNumber [j+ 1];
-            arrNumber[j + 1] = temp ;
-     }
-   }
+   var arrSort = _.sortBy(arrNumber);
 
-}
-
-document.getElementById('ket-qua-7').innerHTML = arrNumber;
+document.getElementById('ket-qua-7').innerHTML = arrSort;
 
 }
 
@@ -221,7 +175,6 @@ document.getElementById('ket-qua-7').innerHTML = arrNumber;
 document.getElementById('btnTimSoNguyenToDauTien').onclick =  function () {
     var soNguyenTo = 0 ;
     
-   
     for (let i = 0; i < arrNumber.length; i++) {
       let isPrime = true;
      
@@ -229,14 +182,11 @@ document.getElementById('btnTimSoNguyenToDauTien').onclick =  function () {
         if (arrNumber[i] % j === 0) {
           isPrime = false;
           break;
-          
         }
       }
       if (isPrime && arrNumber[i] > 1) {
         soNguyenTo = arrNumber[i];
-        
         break;
-        
       }
     }
     document.getElementById('ket-qua-8').innerHTML  = soNguyenTo;
@@ -257,8 +207,6 @@ document.getElementById('btnThemSoRandom').onclick = function () {
 document.getElementById('btnDemSoNguyen').onclick = function () {
 
     var soNguyen = 0;
-    
-
     for(var index= 0 ;index < arrNumber.length;index++) {
 
         if (Number.isInteger(arrNumber[index])) {
@@ -269,8 +217,6 @@ document.getElementById('btnDemSoNguyen').onclick = function () {
     }
     document.getElementById('ket-qua-9').innerHTML = soNguyen;
     
-
-
 }
 //so sánh số lượng số âm và số dương
 
@@ -280,14 +226,12 @@ document.getElementById('btnSoSanh2So').onclick = function () {
     var soAm = 0 ;
     var soDuong = 0;
     var htmlString = '';
-
     for (var index = 0 ; index < arrNumber.length;index++) {
         if (arrNumber[index]> 0 ) {
             soDuong++;
         }else if (arrNumber[index] < 0) {
             soAm++;
         }
-
         if(soDuong === soAm) {
             htmlString = 'Số dương bằng số âm';
         }else if (soDuong < soAm) {
@@ -296,8 +240,6 @@ document.getElementById('btnSoSanh2So').onclick = function () {
             htmlString = 'Số dương nhiều hơn số âm';
         }
     }
-
-
    document.getElementById('ket-qua-10').innerHTML = htmlString; 
 
   }
